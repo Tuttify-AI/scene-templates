2f14d97fc22afc5452cc763eb167b598bf7d9397
sergii-severynov <sergiisgmail.com>
2022-06-29 17:21:30 +0300

    minor bug fixes for word guess

--- src/templates/wordGuess/utils.ts
+++ src/templates/wordGuess/utils.ts
@@ -1,4 +1,7 @@
 export function checkArray(answer_arr: (number | null)[]) {
+  if (!answer_arr?.length) {
+    return false;
+  }
   for (let i = 0; i < answer_arr.length; i++) {
     const index = answer_arr[i];
     if (!index && typeof index != 'number') {
