2d9da76ba45f41bd482bf5fa67dd7dbff2a7cf42
sergii-severynov <sergiisgmail.com>
2022-07-14 18:33:29 +0300

    updated parallax scene

--- src/templates/parallax1/constants.ts
+++ src/templates/parallax1/constants.ts
@@ -2,17 +2,20 @@ import { Image } from './types';
 import image1 from './assets/image1';
 import image2 from './assets/image2';
 import image3 from './assets/image3';
+import image4 from './assets/image4';
+import background from './assets/background';
 
 export const ANIMATIONS: Image[] = [
   {
     name: 'background1',
-    mods: { modX: 20, modY: 20 },
+    mods: { modX: -40, modY: 40 },
+    defaultImage: background,
   },
   {
     name: 'image4',
     isStatic: false,
+    defaultImage: image4,
     mods: { modX: 20, modY: 20 },
-    scale: 1.03,
   },
 ];
 
@@ -27,7 +30,7 @@ export const IMAGES: Image[] = [
     name: 'image2',
     defaultImage: image2,
     isStatic: false,
-    mods: { modX: -2, modY: 20 },
+    mods: { modX: -2, modY: 15 },
   },
   {
     name: 'image3',
