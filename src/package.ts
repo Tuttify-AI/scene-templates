d15c7f167ddcbc5970b79afa100458391efdfff9
atul-tuttifyai <atul@tuttify.ai>
2025-07-01 15:27:10 -0700

    Add 'Multiply Tiles 4' scene
    # Conflicts:
    #	src/templates/multipletiles4/assets/preview.ts
    #	src/templates/multipletiles4/constants.ts
    #	src/templates/multipletiles4/index.tsx
    #	src/templates/multipletiles4/styles.module.css

--- src/package.ts
+++ src/package.ts
@@ -2,6 +2,7 @@ import * as Types from './templates/shared/types';
 import Base, { BaseSceneProps } from './templates/base';
 import Base2, { Base2SceneProps } from './templates/base2';
 import MultipleTiles, { MultipleTilesSceneProps } from './templates/multipletiles';
+import MultipleTiles4, { MultipleTiles4SceneProps } from './templates/multipletiles4';
 import MultipleTiles6, { MultipleTiles6SceneProps } from './templates/multipletiles6';
 import MultipleTiles8, { MultipleTiles8SceneProps } from './templates/multipletiles8';
 import AtomiHalloween from './templates/atomiHalloween';
@@ -9,6 +10,7 @@ import AtomiHalloween from './templates/atomiHalloween';
 import base from './templates/base/assets/preview';
 import base2 from './templates/base2/assets/preview';
 import multipleTiles from './templates/multipletiles/assets/preview';
+import multipleTiles4 from './templates/multipletiles4/assets/preview';
 import multipleTiles6 from './templates/multipletiles6/assets/preview';
 import multipleTiles8 from './templates/multipletiles8/assets/preview';
 import atomiHalloween from './templates/atomiHalloween/assets/preview';
@@ -17,6 +19,7 @@ const previews = {
   base,
   base2,
   multipleTiles,
+  multipleTiles4,
   multipleTiles6,
   multipleTiles8,
   atomiHalloween,
@@ -29,6 +32,8 @@ export {
   Base2SceneProps,
   MultipleTiles,
   MultipleTilesSceneProps,
+  MultipleTiles4,
+  MultipleTiles4SceneProps,
   MultipleTiles6,
   MultipleTiles6SceneProps,
   MultipleTiles8,
@@ -36,4 +41,4 @@ export {
   AtomiHalloween,
   Types,
   previews
-}
+};
